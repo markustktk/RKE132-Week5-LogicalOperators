a6b076f9ca56b8fba2ac082ee5fead96cbdc7a03
Markus Rei. <markus.reinumae@tktk.ee>
2024-05-01 17:16:19 +0300

    Week5LogicalOperators

---
+++ OROperator/Program.cs
@@ -0,0 +1,22 @@
+﻿// Math >=90 OR Chemistry >=90;
+//Math >= 90, Biology >= 90, Chemistry >=90;
+
+int math, biology, chemistry;
+
+Console.WriteLine("Enter your Math result: ");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemistry result: ");
+chemistry = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemistry result: ");
+biology = Int32.Parse(Console.ReadLine());
+
+if ((math >= 90 && chemistry >=90) || (math >= 90 && biology >=90) || (chemistry >=90 && biology >=90))
+{
+    Console.WriteLine("Congratulations! You got accepted");
+}
+else
+{
+    Console.WriteLine("Your application could not be approved");
+}
---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+//AND - &&
+
+//true && true --: true
+//true && false --: false
+//false && true --: false
+//false && false --: false
+
+//OR || (pipes)
+
+//true || true --> true
+//true || false --> true
+//false || true --> true
+//false || false --> false
+
+
+
+//Math >= 90, Biology >= 90, Chemistry >=90;
